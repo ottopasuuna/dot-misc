b984b9f9050aa9c59be5d7e4a55abc2de68dfb72
Carl Hofmeister <hofmeister.carl@gmail.com>
2018-11-01 00:15:19 -0600

    Moved misc bin scripts to dot-misc

--- config.sh
+++ config.sh
@@ -1,5 +1,10 @@
 description="Miscellaneous config files"
 
+ensure_dirs=(
+    $HOME/.config/termite
+    $HOME/.config/mpd
+)
+
 link_map=(
     [tmux.conf]=$HOME/.tmux.conf
     [gdbinit]=$HOME/.gdbinit
@@ -8,9 +13,5 @@ link_map=(
     [termite_config]=$HOME/.config/termite/config
     [mpd.conf]=$HOME/.config/mpd/mpd.conf
     [ranger]=$HOME/.config/ranger
+    [bin]=$HOME/bin
 )
-
-__preinstall__() {
-    mkdir -p $HOME/.config/termite
-    mkdir -p $HOME/.config/mpd
-}
